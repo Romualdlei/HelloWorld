b49f781fbc4823b672ce61142d105608c2bba2d7
Romualds Leitans <Romualdlei@gmail.com>
2022-11-08 11:28:54 +0000

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.myname.service;
+
+public class HelloWorld {
+    public static final String ATTA = "";
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!" + "How are you doing?");
+        System.out.println("HI");
+        System.out.println("Abra kadabra");
+    }
+}
